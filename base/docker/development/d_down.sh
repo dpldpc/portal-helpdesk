fe5e665e69057280e349b2477f258426600d9ffe
David de Souza <davids@vidi.com.br>
2025-06-13 00:57:16 -0300

    Remove old base subtree

--- base/docker/development/d_down.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-LOCALDIR=$(dirname "$0")
-cd $LOCALDIR
-
-export DOCKER_SOCKET_PATH=$(echo $DOCKER_HOST | sed 's|unix://||')
-
-docker compose down
--- base/scripts/push-base-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git subtree push --prefix=base-docker git@github.com:dpldpc/base-docker.git main
-if [ $? -ne 0 ]; then
-    echo "Failed to push base-docker subtree."
-    exit 1
-fi
-echo "Successfully pushed base-docker subtree."
